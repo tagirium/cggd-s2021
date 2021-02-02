70ed9c111a1fd9bc23681e8f6cc59a524145d086
tagir <tagir@DESKTOP-QHSACCD>
2021-02-02 15:44:29 +0300

    week 1

---
+++ src/resource.h
@@ -0,0 +1,142 @@
+#pragma once
+
+#include "utils/error_handler.h"
+
+#include <algorithm>
+#include <linalg.h>
+#include <vector>
+
+
+using namespace linalg::aliases;
+
+namespace cg
+{
+template<typename T>
+class resource
+{
+public:
+	resource(size_t size);
+	resource(size_t x_size, size_t y_size);
+	~resource();
+
+	const T* get_data();
+	T& item(size_t item);
+	T& item(size_t x, size_t y);
+
+	size_t get_size_in_bytes() const;
+	size_t get_number_of_elements() const;
+	size_t get_stride() const;
+
+private:
+	std::vector<T> data;
+	size_t item_size = sizeof(T);
+	size_t stride;
+};
+template<typename T>
+inline resource<T>::resource(size_t size)
+{
+//THROW_ERROR("Not implemented yet");
+	data.resize(size);
+	//item_size = sizeof(T);
+	stride = 0;
+}
+template<typename T>
+inline resource<T>::resource(size_t x_size, size_t y_size)
+{
+	//THROW_ERROR("Not implemented yet");
+	data.resize(x_size * y_size);
+	//item_size = sizeof(T);
+	stride = x_size;
+}
+template<typename T>
+inline resource<T>::~resource()
+{
+}
+template<typename T>
+inline const T* resource<T>::get_data()
+{
+	return data.data();
+}
+template<typename T>
+inline T& resource<T>::item(size_t item)
+{
+	return data.at(item);
+}
+template<typename T>
+inline T& resource<T>::item(size_t x, size_t y)
+{
+	return data.at(y * stride + x);
+}
+template<typename T>
+inline size_t resource<T>::get_size_in_bytes() const
+{
+	return data.size() * item_size;
+}
+template<typename T>
+inline size_t resource<T>::get_number_of_elements() const
+{
+	return data.size();
+}
+
+template<typename T>
+inline size_t resource<T>::get_stride() const
+{
+	return stride;
+}
+struct color
+{
+	static color from_float3(const float3& in)
+	{
+		THROW_ERROR("Not implemented yet");
+		return color();
+	};
+	float r;
+	float g;
+	float b;
+};
+
+struct unsigned_color
+{
+	static unsigned_color from_color(const color& color)
+	{
+		unsigned_color out{};
+		out.r = std::clamp(static_cast<int>(255.f * color.r), 0, 255);
+		out.g = std::clamp(static_cast<int>(255.f * color.g), 0, 255);
+		out.b = std::clamp(static_cast<int>(255.f * color.b), 0, 255);
+		return out;
+	};
+	float3 to_float3()
+	{
+		THROW_ERROR("Not implemented yet");
+		return float3();
+	};
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+
+struct vertex
+{
+	float x;
+	float y;
+	float z;
+
+	float nx;
+	float ny;
+	float nz;
+
+	float ambient_r;
+	float ambient_g;
+	float ambient_b;
+
+	float diffuse_r;
+	float diffuse_g;
+	float diffuse_b;
+
+	float emissive_r;
+	float emissive_g;
+	float emissive_b;
+};
+
+} // namespace cg
---
+++ src/world/camera.cpp
@@ -0,0 +1,135 @@
+#define _USE_MATH_DEFINES
+
+#include "camera.h"
+
+#include "utils/error_handler.h"
+
+#include <math.h>
+
+
+using namespace cg::world;
+
+cg::world::camera::camera() :
+theta(0.f), phi(0.f), height(1080.f), width(1920.f), aspect_ratio(1920.f / 1080.f), angle_of_view(1.04719f), z_near(0.001f),
+z_far(100.f), position(float3{ 0.f, 0.f, 0.f })
+{
+}
+
+cg::world::camera::~camera() {}
+
+void cg::world::camera::set_position(float3 in_position)
+{
+	position = in_position;
+}
+
+void cg::world::camera::set_theta(float in_theta)
+{
+	theta = in_theta * static_cast<float>(M_PI)/180.f;
+}
+
+void cg::world::camera::set_phi(float in_phi)
+{
+	phi = in_phi * static_cast<float>(M_PI) / 180.f;
+}
+
+void cg::world::camera::set_angle_of_view(float in_aov)
+{
+	angle_of_view = in_aov * static_cast<float>(M_PI) / 180.f;
+}
+
+void cg::world::camera::set_height(float in_height)
+{
+	height = in_height;
+	aspect_ratio = width / height;
+}
+
+void cg::world::camera::set_width(float in_width)
+{
+	width = in_width;
+	aspect_ratio = width / height;
+}
+
+void cg::world::camera::set_z_near(float in_z_near)
+{
+	z_near = in_z_near;
+}
+
+void cg::world::camera::set_z_far(float in_z_far)
+{
+	z_far = in_z_far;
+}
+
+const float4x4 cg::world::camera::get_view_matrix() const
+{
+	float3 up{ 0.f, 1.f, 0.f };
+	float3 eye = position + get_direction();
+	float3 z_axis = normalize(position - eye);
+	float3 x_axis = normalize(cross(up, z_axis));
+	float3 y_axis = cross(z_axis, x_axis);
+	/*return float4x4
+	{
+		{ x_axis.x, x_axis.y, x_axis.z, 0 }, { y_axis.x, y_axis.y, y_axis.z, 0 },
+			{ z_axis.x, z_axis.y, z_axis.z, 0 },
+			{ -dot(x_axis, position), -dot(y_axis, position),
+			  -dot(z_axis, position), 1 };
+	*/
+	return float4x4{ { x_axis.x, y_axis.x, z_axis.x, 0 },
+						 { x_axis.y, y_axis.y, z_axis.y, 0 },
+						 { x_axis.z, y_axis.z, z_axis.z, 0 },
+						 { -dot(x_axis, position), -dot(y_axis, position),
+						   -dot(z_axis, position), 1 }
+	};
+}
+
+#ifdef DX12
+const DirectX::XMMATRIX cg::world::camera::get_dxm_view_matrix() const
+{
+	float3 direction = get_direction();
+	DirectX::FXMVECTOR up_direction{ 0.f, 1.f, 0.f };
+	DirectX::FXMVECTOR eye_direction{ direction.x, direction.y, direction.z };
+	DirectX::FXMVECTOR eye_position{ position.x, position.y, position.z };
+	return DirectX::XMMatrixLookToRH(eye_position, eye_direction, up_direction);
+	//return DirectX::XMMatrixIdentity();
+}
+
+const DirectX::XMMATRIX cg::world::camera::get_dxm_projection_matrix() const
+{
+	//float3 direction = get_direction();
+	//DirectX::FMXVECTOR up_direction{ 0.f, 1.f, 0.f };
+	//DirectX::FMXVECTOR eye_direction{ direction.x, direction.y, direction.x};
+	//DirectX::FMXVECTOR eye_position{ position.x, position.y, position.x };
+	//return DirectX::XMMAtrixLookToRH(
+		//eye_position, eye_direction, up_direction)
+	return DirectX::XMMatrixIdentity();
+}
+#endif
+
+const float4x4 cg::world::camera::get_projection_matrix() const
+{
+	float f = 1.f / std::tanf(angle_of_view / 2.f);
+
+	return float4x4 { { f / aspect_ratio, 0, 0, 0 },
+					 { 0, f, 0, 0 },
+					 { 0, 0, z_far / (z_near - z_far), -1 },
+					 { 0, 0, (z_far * z_near) / (z_near - z_far), 0} };
+}
+
+const float3 cg::world::camera::get_position() const
+{
+	return position;
+}
+
+const float3 cg::world::camera::get_direction() const
+{
+	return float3{std::sin(theta) * std::cos(phi), -std::sin(phi), -std::cos(theta) * std::cos(phi)};
+}
+
+const float3 cg::world::camera::get_right() const
+{
+	return cross(get_direction(), float3{0.f, 1.f, 0.f});
+}
+
+const float3 cg::world::camera::get_up() const
+{
+	return cross(get_right(), get_direction());
+}
